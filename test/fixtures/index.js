48ca24770a156b68ad95b8f7509951324f1d475f
Alexis Araujo <ala1289@gmail.com>
2017-04-17 16:32:40 -0500

    Creación de nuestro endpoint GET /picture

---
+++ test/fixtures/index.js
@@ -0,0 +1,13 @@
+export default {
+  getImage () {
+    return {
+      id: '67e8a91f-c348-4575-8e8c-356c0c58564c',
+      publicId: '3LBTou7pRI3kRGY9VeRsRv',
+      userId: 'ciberdarc',
+      src: 'http://platzigram.test/3LBTou7pRI3kRGY9VeRsRv.jpg',
+      description: '#awesome',
+      tag: ['awesome'],
+      createdAt: new Date().toString()
+    }
+  }
+}
