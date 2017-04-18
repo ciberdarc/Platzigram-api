3b2486e857b093f18251e7a60c6c418b67ef910e
Alexis Araujo <ala1289@gmail.com>
2017-04-18 09:04:03 -0500

    Creción de nuestro endpoint POST /picture/:id/like

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -4,6 +4,8 @@ export default {
       id: '67e8a91f-c348-4575-8e8c-356c0c58564c',
       publicId: '3LBTou7pRI3kRGY9VeRsRv',
       userId: 'platzigram',
+      liked: false,
+      likes: 0,
       src: 'http://platzigram.test/3LBTou7pRI3kRGY9VeRsRv.jpg',
       description: '#awesome',
       tag: ['awesome'],
