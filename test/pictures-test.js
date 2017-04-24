7a3fd76a34c16ed118831670b56c961fbb714542
Alexis Araujo <ala1289@gmail.com>
2017-04-24 08:02:31 -0500

    Limitando nuestros request esperando recibir un token

--- test/pictures-test.js
+++ test/pictures-test.js
@@ -6,6 +6,8 @@ import listen from 'test-listen'
 import request from 'request-promise'
 import pictures from '../pictures'
 import fixtures from './fixtures'
+import utils from '../lib/utils'
+import config from '../config'
 
 test.beforeEach(async t => {
   let srv = micro(pictures)
@@ -19,7 +21,7 @@ test('GET /:id', async t => {
   t.deepEqual(body, image)
 })
 
-test('POST /', async t => {
+test('no token POST /', async t => {
   let image = fixtures.getImage()
   let url = t.context.url
 
@@ -34,9 +36,53 @@ test('POST /', async t => {
     },
     resolveWithFullResponse: true
   }
+
+  return t.throws(request(options), /invalid token/)
+})
+
+test('secure POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+  let token = await utils.signToken({userId: image.userId}, config.secret)
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    resolveWithFullResponse: true
+  }
   let response = await request(options)
+
   t.is(response.statusCode, 201)
   t.deepEqual(response.body, image)
+})
+
+test('invalid token POST /', async t => {
+  let image = fixtures.getImage()
+  let url = t.context.url
+  let token = await utils.signToken({userId: 'hacky'}, config.secret)
+  let options = {
+    method: 'POST',
+    uri: url,
+    json: true,
+    body: {
+      description: image.description,
+      src: image.src,
+      userId: image.userId
+    },
+    headers: {
+      'Authorization': `Bearer ${token}`
+    },
+    resolveWithFullResponse: true
+  }
+  return t.throws(request(options), /invalid token/)
 })
 
 test('POST /:id/like', async t => {
